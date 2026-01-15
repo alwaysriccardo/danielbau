3edbd14331f7ac14102b10caa5415972d49f002a
Riccardo <riccardo@Riccardos-MacBook-Pro.local>
2026-01-16 01:33:40 +0200

    Fix map iframe scroll interference on mobile: prevent map from capturing scroll gestures

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -193,12 +193,21 @@ const Footer: React.FC = () => {
 
           {/* Small Square Map */}
           <div className="flex-1 max-w-md">
-            <div className="w-full h-[250px] md:h-[300px] rounded-lg overflow-hidden border border-white/20 shadow-lg">
+            <div 
+              className="w-full h-[250px] md:h-[300px] rounded-lg overflow-hidden border border-white/20 shadow-lg map-container"
+              style={{ 
+                position: 'relative',
+                zIndex: 10
+              }}
+            >
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2708.5!2d7.52065!3d47.2794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDfCsDE2JzQ1LjgiTiA3wrAzMScxNC4zIkU!5e0!3m2!1sen!2sch!4v1234567890&q=Rheistrasse+3,+4410+Liestal+Switzerland|Fluhrweg+16,+3250+Lyss+Switzerland"
                 width="100%"
                 height="100%"
-                style={{ border: 0 }}
+                style={{ 
+                  border: 0,
+                  pointerEvents: 'auto'
+                }}
                 allowFullScreen
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
