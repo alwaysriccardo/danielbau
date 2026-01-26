eb86c1a4379b99fe7e686ac90dc0de6d935f7648
Riccardo <riccardo@Riccardos-MacBook-Pro.local>
2026-01-26 21:30:31 +0200

    Fix import path for LanguageContext in PromoPopup

--- components/PromoPopup.tsx
+++ components/PromoPopup.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLanguage } from './contexts/LanguageContext';
+import { useLanguage } from '../contexts/LanguageContext';
 
 interface PromoPopupProps {
   onClose: () => void;
